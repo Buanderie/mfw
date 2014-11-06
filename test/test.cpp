bea711c56fa3951d3f63722816b9ee144a6c9c0d
Nicolas Said <said@said-laptop.(none)>
2014-11-06 18:35:55 +0100

    Addition of gtest for future use...

---
+++ test/test.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+
+#include <gtest/gtest.h>
+
+TEST(cpp_sorter_test, null_term_str_sort)
+{
+	EXPECT_EQ(7, 7);
+}
+
+TEST(cpp_sorter_test, char_arr_sort)
+{
+EXPECT_EQ(7, 7);
+}
+
+TEST(cpp_sorter_test, int_arr_sort)
+{
+EXPECT_EQ(7, 7);
+}
